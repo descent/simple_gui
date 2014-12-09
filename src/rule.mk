80b8e680b771c7ca7eeff205434274e360552473
descent <descent0@gmail.com>
2014-12-09 16:06:08 +0800

    rule for ccache

--- src/rule.mk
+++ src/rule.mk
@@ -3,6 +3,12 @@ MYCFLAGS=
 MYCXXFLAGS =
 LD_FLAGS=
 CXX=g++
+
+# make CH=1 will use ccache
+ifdef CH
+CXX=ccache g++
+endif
+
 %.dpp: %.cpp
 	set -e; rm -f $@ ; $(CXX) -MM $(MYCFLAGS) $< > $@.$$$$ ; sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@ ; rm -f $@.$$$$
 
